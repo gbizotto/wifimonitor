475b86a7cef7b0cc91baf714839c002d1ef1bde2
gabriela.bizotto <gbizotto@gmail.com>
2021-01-06 09:40:37 -0300

    adding coroutines

--- app/src/main/java/com/gbizotto/wifimonitor/datasource/ConnectionLoggerDataSource.kt
+++ app/src/main/java/com/gbizotto/wifimonitor/datasource/ConnectionLoggerDataSource.kt
@@ -1,17 +1,13 @@
 package com.gbizotto.wifimonitor.datasource
 
-import android.os.Handler
-import android.os.Looper
 import com.gbizotto.wifimonitor.data.ConnectionLog
 import com.gbizotto.wifimonitor.data.ConnectionLogDao
-import java.util.concurrent.ExecutorService
-import java.util.concurrent.Executors
 import javax.inject.Inject
 import javax.inject.Singleton
 
 interface ConnectionLoggerDataSource {
     suspend fun addLog(connected: Boolean)
-    suspend fun getAllLogs(callback: (List<ConnectionLog>) -> Unit)
+    suspend fun getAllLogs(): List<ConnectionLog>
     fun removeLogs()
 }
 
@@ -20,28 +16,15 @@ class ConnectionLoggerLocalDataSource @Inject constructor(
     private val logDao: ConnectionLogDao
 ) : ConnectionLoggerDataSource {
 
-    private val executorService: ExecutorService = Executors.newFixedThreadPool(4)
-    private val mainThreadHandler by lazy {
-        Handler(Looper.getMainLooper())
-    }
-
     override suspend fun addLog(connected: Boolean) {
-//        executorService.execute {
-            logDao.insertAll(
-                ConnectionLog(connected, System.currentTimeMillis())
-            )
-//        }
+        logDao.insertAll(
+            ConnectionLog(connected, System.currentTimeMillis())
+        )
     }
 
-    override suspend fun getAllLogs(callback: (List<ConnectionLog>) -> Unit) {
-        executorService.execute {
-            val logs = logDao.getAll()
-            mainThreadHandler.post { callback(logs) }
-        }
-    }
+    override suspend fun getAllLogs(): List<ConnectionLog> = logDao.getAll()
 
     override fun removeLogs() {
         TODO("Not yet implemented")
     }
-
 }
--- app/src/main/java/com/gbizotto/wifimonitor/usecase/GetAllLogsUseCase.kt
+++ app/src/main/java/com/gbizotto/wifimonitor/usecase/GetAllLogsUseCase.kt
@@ -5,18 +5,11 @@ import com.gbizotto.wifimonitor.datasource.ConnectionLoggerDataSource
 import javax.inject.Inject
 
 interface GetAllLogsUseCase {
-    operator fun invoke(): List<ConnectionLog>
+    suspend operator fun invoke(): List<ConnectionLog>
 }
 
 class GetAllLogs @Inject constructor(
     private val connectionLoggerDataSource: ConnectionLoggerDataSource,
 ) : GetAllLogsUseCase {
-    override fun invoke(): List<ConnectionLog> {
-        TODO()
-//        var list: List<ConnectionLog>
-//        connectionLoggerDataSource.getAllLogs {
-//            list = it
-//        }
-//        return list
-    }
+    override suspend fun invoke(): List<ConnectionLog> = connectionLoggerDataSource.getAllLogs()
 }
